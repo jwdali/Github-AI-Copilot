fc5f3f4f084855d8557235f6c0418541fa54c13b
walt-instructor <staffauthor@outlook.com>
2025-09-23 20:48:54 -0700

    add comment about how to insert comments with inline chat.

--- code/Inline/OlderLib/ImperativeCode.cs
+++ code/Inline/OlderLib/ImperativeCode.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Models;
 
 namespace OlderLib {
 	internal class ImperativeCode {
+		// Write inline comments for this code(not XML Doc comments)
 		public List<Tour> GetActiveTours(List<Tour> tours) {
+
 			var result = new List<Tour>();
 			foreach (var tour in tours)
 			{
@@ -18,8 +21,9 @@ namespace OlderLib {
 			}
 			return result;
 		}
-
+		// /explain annotate with inline // comments
 		public List<string> GetShortBeginnerTourNames(List<Tour> tours) {
+
 			var result = new List<string>();
 			foreach (var tour in tours)
 			{
