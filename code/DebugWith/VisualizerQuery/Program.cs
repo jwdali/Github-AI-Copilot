843d5db82b94e1ccd3a638278f424ee637576ff9
walt-instructor <staffauthor@outlook.com>
2025-09-28 21:29:46 -0700

    update writeline

--- code/DebugWith/VisualizerQuery/Program.cs
+++ code/DebugWith/VisualizerQuery/Program.cs
@@ -9,7 +9,7 @@
 		}
 
 		static void TryGreen() {
-			Console.WriteLine("Green Colors Demo:");
+			Console.WriteLine("Very Green Colors:");
 			var worker = new WorkWithLists();
 			var greens = worker.GetGreenColors();
 			foreach (var color in greens)
@@ -18,7 +18,7 @@
 			}
 		}
 		static void TrySaturation() {
-			Console.WriteLine("SaturatedColors:");
+			Console.WriteLine("Desaturated Colors:");
 			var worker = new WorkWithLists();
 			var colors = worker.GetLowSaturationColors();
 			foreach (var color in colors)
